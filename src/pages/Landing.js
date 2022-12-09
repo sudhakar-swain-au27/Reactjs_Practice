fd5c9a59c09147f68123881cd3029203d499f7b6
Sudhakar Swain <91896763+sudhakar-swain-au27@users.noreply.github.com>
2022-12-09 17:17:20 +0530

    Project Done

---
+++ src/pages/Landing.js
@@ -0,0 +1,30 @@
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Make your recruiter life Easy and manage your time with JOB Street.
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
